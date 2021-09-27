f6770e815542aa12fcaf7fcc6e9e6d5c10288bea
golibjon94 <golib94jon@gmail.com>
2021-09-27 19:03:31 +0500

    I prepared the sidebar part of the project css styles

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,8 +7,26 @@ import logo from '../../assets/images/logo.png'
 import sidebar_items from '../../assets/JsonData/sidebar_routes.json';
 import { Link } from 'react-router-dom';
 
+ const SidebarItem = props => {
+     const active = props.active ? 'active' : ''
 
-const Sidebar = () => {
+     return (
+         <div className="sidebar__item">
+             <div className={`sidebar__item-inner ${active}`}>
+                <i className={props.icon}></i>
+                <span>
+                    {props.title }
+                </span>
+             </div>
+         </div>
+     )
+ }
+
+
+const Sidebar = props => {
+
+    const activeItem = sidebar_items.findIndex(item => item.route === props.location.pathname)
+
     return (
         <div className='sidebar'>
             <div className="sidebar__logo">
@@ -17,9 +35,11 @@ const Sidebar = () => {
             {
                 sidebar_items.map((item, index) => (
                     <Link to={item.route} key={index}>
-                        <div>
-                            {item.display_name}
-                        </div>
+                      <SidebarItem
+                        title={item.display_name}
+                        icon={item.icon}
+                        avtive={index === activeItem}
+                      />
                     </Link>
                 ))
             }
